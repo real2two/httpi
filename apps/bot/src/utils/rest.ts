c4965fc3f5cf2868fa843dc30ad4f28c5275712e
Two <26689496+real2two@users.noreply.github.com>
2024-05-17 07:18:15 -0400

    Removed an unnecessary function (originally created to be consistent with respond() but I decided I'll just use djs's core.)

--- apps/bot/src/utils/rest.ts
+++ apps/bot/src/utils/rest.ts
@@ -12,38 +12,3 @@ import type {
 
 export const rest = new REST({ version: '10' }).setToken(env.DiscordToken);
 export const discord = new API(rest);
-
-export async function editMessage(
-  interaction: CamelizedInteraction,
-  message: InteractionUpdateResponse,
-) {
-  if (message?.attachments) {
-    // Create attachments variable
-    const attachments = message?.attachments as InteractionResponseAttachment[];
-    // Create an updated message attachments object
-    const messageAttachments: RESTAPIAttachment[] = [];
-    for (let id = 0; id < message.attachments.length; ++id) {
-      messageAttachments.push({
-        id,
-        filename: message.attachments[id].name,
-      });
-    }
-    // Create attachment response
-    return objectToCamel(
-      (await rest.patch(Routes.webhookMessage(interaction.applicationId, interaction.token), {
-        body: {
-          ...objectToSnake(message),
-          attachments: messageAttachments,
-        },
-        files: attachments,
-      })) as Promise<CamelizedRESTPatchAPIWebhookResult>,
-    );
-  } else {
-    // Create normal response
-    return objectToCamel(
-      (await rest.patch(Routes.webhookMessage(interaction.applicationId, interaction.token), {
-        body: objectToSnake(message),
-      })) as Promise<CamelizedRESTPatchAPIWebhookResult>,
-    );
-  }
-}
