6c49814d436554c22bfbd9dd0d4935f1ef43c5d8
Two <26689496+real2two@users.noreply.github.com>
2024-05-17 11:32:11 +0000

    Removed unnecessary imports

--- apps/bot/src/utils/rest.ts
+++ apps/bot/src/utils/rest.ts
@@ -1,14 +1,6 @@
 import env from '@/env';
-import { objectToSnake, objectToCamel } from 'ts-case-convert';
 import { API } from '@discordjs/core';
 import { REST } from '@discordjs/rest';
-import { RESTAPIAttachment, Routes } from 'discord-api-types/v10';
-import type {
-  CamelizedInteraction,
-  InteractionUpdateResponse,
-  CamelizedRESTPatchAPIWebhookResult,
-  InteractionResponseAttachment,
-} from '@/discord';
 
 export const rest = new REST({ version: '10' }).setToken(env.DiscordToken);
 export const discord = new API(rest);
