a9a8c45600804e6716c00e24d2dfff561653e68d
real2two <26689496+real2two@users.noreply.github.com>
2024-05-16 22:20:10 -0400

    Added options example

--- apps/bot/src/commands/subcommand/test.ts
+++ apps/bot/src/commands/subcommand/test.ts
@@ -1,16 +1,24 @@
 import { Subcommand } from '@/discord';
-import { InteractionResponseType } from 'discord-api-types/v10';
+import { ApplicationCommandOptionType, InteractionResponseType } from 'discord-api-types/v10';
 
 export default new Subcommand({
   data: {
     name: 'test',
     description: 'This is a test subcommand',
+    options: [
+      {
+        type: ApplicationCommandOptionType.Boolean,
+        name: 'value',
+        description: 'This is a test boolean',
+        required: true,
+      },
+    ],
   },
-  execute: ({ respond }) => {
+  execute: ({ interaction, respond }) => {
     respond({
       type: InteractionResponseType.ChannelMessageWithSource,
       data: {
-        content: 'This is a test subcommand',
+        content: `This is a test subcommand. The given value is \`${interaction.data.options[0].options[0].value}\`.`,
       },
     });
   },
