fdf97930983cf33e77ac837298868b2440c1d63a
real2two <26689496+real2two@users.noreply.github.com>
2024-05-18 21:54:08 -0400

    Update index.ts

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,6 +1,6 @@
 export default {
-  WebsitePort: parseInt(process.env.WEBSITE_PORT!) || 0,
-  WebsiteClusters: parseInt(process.env.WEBSITE_CLUSTERS!) || 0,
+  WebsitePort: parseInt(process.env.WEBSITE_PORT || '0'),
+  WebsiteClusters: parseInt(process.env.WEBSITE_CLUSTERS || '0'),
 
   DiscordClientId: process.env.DISCORD_CLIENT_ID!,
   DiscordPublicKey: process.env.DISCORD_PUBLIC_KEY!,
